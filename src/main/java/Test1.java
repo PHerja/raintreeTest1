5781625e7dadb609cc6b6e07c55e61ec11dd1550
Priit Herja <priit218@gmail.com>
2020-08-18 15:41:54 +0300

    prime number done

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -4,40 +4,57 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Test1 {
-    private static List<Long> list=new ArrayList<>();
+    //private static final int RECORD_COUNT=3380000;
+    private static final int RECORD_COUNT = 3380000;
+
+    private static List<Long> list = new ArrayList<>();
     private static File file;
 
     public static void main(String[] args) throws IOException {
         createFile();
         saveIntoFile();
-        long start=System.currentTimeMillis();
+        long start = System.currentTimeMillis();
         loadFromFile();
-        long end= System.currentTimeMillis();
-        System.out.println((end-start) / 1000f + " seconds to read file.");
+        long end = System.currentTimeMillis();
+        System.out.println((end - start) / 1000f + " seconds to read the file.");
+        long start1 = System.currentTimeMillis();
+        barChart();
+        long end1 = System.currentTimeMillis();
+        System.out.println((end1 - start1) / 1000f + " seconds to calculate bar chart.");
+        long start2 = System.currentTimeMillis();
+        countOfPrimeNumbersBigInt();
+        long end2 = System.currentTimeMillis();
+        System.out.println((end2 - start2) / 1000f + " seconds to calculate Prime numbers.");
+        long start3 = System.currentTimeMillis();
+        countOfArmstrongNumbers();
+        long end3 = System.currentTimeMillis();
+        System.out.println((end3 - start3) / 1000f + " seconds to calculate Armstrong numbers.");
 
     }
+
     private static void createFile() {
-        Scanner input= new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter filename: ");
-        String fileName=input.next();
+        String fileName = input.next();
         input.close();
         setFile(new File(fileName + ".txt"));
     }
 
-    private static long randomGenerator(){
-        return 1+(long)(Math.random() * (Long.MAX_VALUE - 1));
+    private static long randomGenerator() {
+        return 1 + (long) (Math.random() * (Long.MAX_VALUE - 1));
     }
 
     private static void saveIntoFile() throws IOException {
         FileWriter writer = new FileWriter(file);
-        int i=0;
-        while ( i<3380000){
-            writer.write(randomGenerator() +" ");
+        int i = 0;
+        while (i < RECORD_COUNT) {
+            writer.write(randomGenerator() + " ");
             i++;
         }
         writer.close();
@@ -45,25 +62,34 @@ public class Test1 {
 
     private static void loadFromFile() throws FileNotFoundException {
         Scanner sc = new Scanner(file);
-        while (sc.hasNext()){
+        while (sc.hasNext()) {
             list.add(sc.nextLong());
         }
         sc.close();
     }
 
-    public static void setFile(File file) {
+    private static void setFile(File file) {
         Test1.file = file;
     }
 
-    public static void barChart(){
+    public static void barChart() {
 
     }
 
-    public static void countOfPrimeNumbers(){
-
+    public static void countOfArmstrongNumbers() {
+        List<Long> armstrongNumbersList = new ArrayList<>();
     }
 
-    public static void countOfArmstrongNumbers(){
-
+    public static void countOfPrimeNumbersBigInt(){
+        List<Long> primeNumbersList = new ArrayList<>();
+        BigInteger bigInteger;
+        for (long index : list) {
+            bigInteger=BigInteger.valueOf(index);
+            if (bigInteger.isProbablePrime(5)) {
+                primeNumbersList.add(index);
+            }
+        }
+        System.out.println("count of prime numbers= " + primeNumbersList.size());
     }
+
 }
