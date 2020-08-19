f752a4035df1a0e2638faad9ba291ad217425ddd
Priit Herja <priit218@gmail.com>
2020-08-19 21:47:57 +0300

    new features

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -5,16 +5,12 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.function.Function;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Test1 {
     //private static final int RECORD_COUNT=3380000;
-    private static final int RECORD_COUNT = 3380000;
+    private static final int RECORD_COUNT = 33800;
 
     private static List<Long> list = new ArrayList<>();
     private static File file;
@@ -27,11 +23,11 @@ public class Test1 {
         long end = System.currentTimeMillis();
         System.out.println((end - start) / 1000f + " seconds to read the file.");
         long start1 = System.currentTimeMillis();
-        barChart();
+        //barChart();
         long end1 = System.currentTimeMillis();
         System.out.println((end1 - start1) / 1000f + " seconds to calculate bar chart.");
         long start2 = System.currentTimeMillis();
-        countOfPrimeNumbersBigInt();
+        countOfPrimeNumbers();
         long end2 = System.currentTimeMillis();
         System.out.println((end2 - start2) / 1000f + " seconds to calculate Prime numbers.");
         long start3 = System.currentTimeMillis();
@@ -52,12 +48,15 @@ public class Test1 {
     private static long randomGenerator() {
         return 1 + (long) (Math.random() * (Long.MAX_VALUE - 1));
     }
+    private static long randomGenerator2() {
+        return 1 + (long) (Math.random() * (100000 - 1));
+    }
 
     private static void saveIntoFile() throws IOException {
         FileWriter writer = new FileWriter(file);
         int i = 0;
         while (i < RECORD_COUNT) {
-            writer.write(randomGenerator() + " ");
+            writer.write(randomGenerator2() + " ");
             i++;
         }
         writer.close();
@@ -75,56 +74,73 @@ public class Test1 {
         Test1.file = file;
     }
 
-    private static void barChart() {
-        Map<Long, Long> frequencyMap =
-                list.stream().collect(Collectors.groupingBy(Function.identity(),
-                        Collectors.counting()));
-        int i=0;
-        for (Map.Entry<Long, Long> entry : frequencyMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-            if (i>=9) break;
-            i++;
+    public static void barChart() {
+        ArrayList <Integer> keys = new ArrayList<>();
+        keys.addAll(tenMostCommon().keySet());
+        Integer[] keyArray = new Integer[]{15, 12, 9, 7, 4, 3, 2, 2, 1, 0};
+        Integer howManyLines = keys.get(0);
+        Integer fixer = howManyLines - 1;
+
+        while (howManyLines > 0) {
+            System.out.print("           ");
+            for (int i = 0; i <= 198; i++) {
+                if (i % 22 == 0 && keys.get(i / 22) - fixer > 0) {
+                    System.out.print("\u2588\u2588");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+            howManyLines--;
+            fixer--;
         }
+        tenMostCommon();
+
+    }
+    public static Map tenMostCommon() {
+        Map<Long, Integer> map = new HashMap<>();
+        for (Long t : Test1.list) {
+            Integer val = map.get(t);
+            map.put(t, val == null ? 1 : val + 1);
+        }
+        Map<Long,Integer> topTen =
+                map.entrySet().stream()
+                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                        .limit(10)
+                        .collect(Collectors.toMap(
+                                Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        return topTen;
+    }
+    private static boolean isArmstrongNumber(long number){
+        long originalNumber = number, remainder, result = 0;
+        while (originalNumber != 0)
+        {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, 3);
+            originalNumber /= 10;
+        }
+        return result == number;
     }
 
-    private static void countOfArmstrongNumbers() {
-        List<Long> armstrongNumbersList = new ArrayList<>();
-        long remainder,sum,digits,temp;
-        for (long index : list) {
-            sum = 0; temp = index; digits = 0;
-            while (temp != 0) {
-                digits++;
-                temp = temp/10;
-            }
-            temp = index;
-            while (temp != 0) {
-                remainder = temp%10;
-                sum = sum + power(remainder, digits);
-                temp = temp/10;
-            }
-            if ( index == sum ){
-                armstrongNumbersList.add(index);
+    public static void countOfArmstrongNumbers() {
+        long count = 0;
+        for (long index : Test1.list) {
+            if(isArmstrongNumber(index)){
+                count++;
             }
         }
-        System.out.println("count of Armstrong numbers= " + armstrongNumbersList.size());
+        System.out.println("count of Armstrong numbers= " + count);
     }
 
-    private static void countOfPrimeNumbersBigInt(){
-        List<Long> primeNumbersList = new ArrayList<>();
+    public static void countOfPrimeNumbers(){
         BigInteger bigInteger;
-        for (long index : list) {
+        int count=0;
+        for (long index : Test1.list) {
             bigInteger=BigInteger.valueOf(index);
             if (bigInteger.isProbablePrime(5)) {
-                primeNumbersList.add(index);
+                count++;
             }
         }
-        System.out.println("count of Prime numbers= " + primeNumbersList.size());
-    }
-    private static long power(long n, long r) {
-        long c, p = 1;
-        for (c = 1; c <= r; c++)
-            p = p*n;
-        return p;
+        System.out.println("count of Prime numbers= " + count);
     }
 
 }
